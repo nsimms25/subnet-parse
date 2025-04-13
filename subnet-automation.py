9313786e1e474204a43ad0088f04bfcece68d55c
Nic Simms <nsimms@asu.edu>
2025-04-13 06:59:03 +0000

    fixed ip subnetting and can now apply to input IP and return proper network IP address

--- subnet-automation.py
+++ subnet-automation.py
@@ -4,7 +4,6 @@ def cidr_to_subnet_mask(cidr: str):
         raise ValueError("CIDR must be between 0 and 32.")
     
     network_value = 32 - cidr
-    #print(network_value)
     
     mask = (0xFFFFFFFF << network_value) & 0xFFFFFFFF
     mask_bytes = format(mask, '032b')
@@ -14,30 +13,29 @@ def get_network_address(client_ip: str):
     ip , cidr = client_ip.split("/")
     octets = ip.split(".")
     network_mask = cidr_to_subnet_mask(cidr)
+    int_cidr = int(cidr)
 
-    first_octet = network_mask[0:8]
-    second_octet = network_mask[8:16]
-    third_octet = network_mask[16:24]
-    fourth_octet = network_mask[24:32]
-
-    print(first_octet)
-    print(second_octet)
-    print(third_octet)
-    print(fourth_octet)
+    mask_bin = (0xFFFFFFFF << (32 - int_cidr)) & 0xFFFFFFFF
     
-    #network_octets = [
-    #    str((network_mask[0:8] >> 24) & 0xFF),
-    #    str((network_mask >> 16) & 0xFF),
-    #    str((network_mask >> 8) & 0xFF),
-    #    str(network_mask & 0xFF)
-    #]
+    network_mask_octets = [
+        str((mask_bin >> 24) & 0xFF),
+        str((mask_bin >> 16) & 0xFF),
+        str((mask_bin >> 8) & 0xFF),
+        str(mask_bin & 0xFF)
+    ]
 
-    #".".join(network_octets)
+    network_octets_str = ".".join(network_mask_octets)
+    network_ip = []
 
-#print("subnet mask 30: ", cidr_to_subnet_mask(30))
-#print("subnet mask 24: ", cidr_to_subnet_mask(24))
-#print("subnet mask 16: ", cidr_to_subnet_mask(16))
+    for i in range(0,4):
+        octet_bytes = int(octets[i]).to_bytes(8, byteorder="big")
+        network_bytes = int(network_mask_octets[i]).to_bytes(8, byteorder="big")
 
-print("network address: ", get_network_address("192.168.24.245/32"))
-#print("network address: ", get_network_address("192.168.24.245/30"))
-#print("network address: ", get_network_address("192.168.24.245/16"))
+        network_ip.append(str(int.from_bytes(octet_bytes, byteorder="big",signed=True) & int.from_bytes(network_bytes, byteorder="big",signed=True)))
+        
+    network_ip_str = ".".join(network_ip)
+    network_ip_str = network_ip_str + f"/{cidr}"
+    
+    return network_ip_str
+
+print("network address: ", get_network_address("192.168.24.9/30"))
